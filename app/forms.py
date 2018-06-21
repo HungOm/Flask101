0aa617ed5e6ed61a448c734f91dd300b8bc66024
Amos <hungomoct19@outlook.com>
2018-06-21 19:19:25 +0800

    Avater and Edit Profile

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired,ValidationError,Email,EqualTo
+from wtforms import (StringField, PasswordField, BooleanField,
+                     SubmitField, TextAreaField)
+from wtforms.validators import (DataRequired, ValidationError,
+                                Email, EqualTo, Length)
 from app.models import User
+
+
+class EditprofileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Sing In')
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators = [DataRequired()])
-    email = StringField('Email',validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators = [DataRequired(),EqualTo('password')])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(),
+                                                             EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
@@ -24,7 +35,3 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError("Please use a different email address.")
-
-
-
-            
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,34 @@
 from werkzeug.urls import url_parse
 from app import app,db
 from flask import render_template,flash,redirect,request, url_for
-from app.forms import LoginForm,RegistrationForm
+from app.forms import LoginForm,RegistrationForm, EditprofileForm
 from flask_login import current_user, login_user, logout_user,login_required
 from app.models import User
+from datetime import datetime
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditprofileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title="Edit Profile",
+                           form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -11,18 +36,18 @@ from app.models import User
 def index():
     user = {'username':'Om'}
     posts =[
-    {'author':{'username':'John'},
-    'body':"beautiful day in Portland!"
+    {'author': {'username': 'John'},
+    'body': "beautiful day in Portland!"
     },
 
-    {'author':{'username':'Susan'},
-    'body':"The avengers movie was so cool!"
+    {'author': {'username': 'Susan'},
+    'body': "The avengers movie was so cool!"
 
     },
     {'author':{'username':'Om'},
-    'body':"Roses are red, Sky are blue, you are nerver a fool to love ,Really?"}
+    'body':"Roses are red, Sky are blue, you are nerver a fool to love, Really?"}
     ]
-    return render_template('index.html', user =user, title ="Home", posts =posts)
+    return render_template('index.html', user=user, title="Home", posts=posts)
 
 
 # @app.route('/hello/<name>')
@@ -80,7 +105,8 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -91,5 +117,19 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for(login))
+        return redirect(url_for('login'))
     return render_template('register.html',title = 'Register', form=form)
+
+
+@app.route('/user/<username>')
+# allow only for authorized users
+@login_required
+# the view for user routes
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+            {'author': user, 'body': 'Test Post #1'},
+            {'author': user, 'body': 'Test Post #2'}
+            ]
+    # render template with the posts above
+    return render_template('user.html', user=user, posts=posts)
